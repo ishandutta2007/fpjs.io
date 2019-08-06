2efbbe0f3fd5910781d141a7d94ffd9db622c2c0
Valentin Vasilyev <valentin@fingerprintjs.com>
2019-08-06 13:07:48 +0300

    [new-demo] improving the demo

--- js/demo.js
+++ js/demo.js
@@ -1,15 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFsdmUxIiwiYSI6ImNqeXUwdHlnejA5YzMzaHBnN3R4OXF1czEifQ.4-Wne3WDiafdfFGLSTkFiQ';
 
-function Visit(response){
-  this.response = response;
-  this.time = response.time;
-  this.incognito = response.incognito;
-  this.formattedIncognito = formatBool(response.incognito);
-  this.ip = response.ip;
-  this.fullLocation = formatIpLocation(response.ipLocation);
-  this.fullBrowser = formatBrowser(response.browserDetails);
-  this.timeAgo = timeago().format(response.time);
-}
 
 var formatIpLocation = function (ipLocation) {
   var loc = ipLocation;
@@ -54,8 +44,61 @@ var formatBrowser = function (browser) {
   return out;
 }
 
-var formatBool = function(value) {
+var formatBool = function (value) {
   return value ? "YES" : "NO";
+}
+var formatBot = function (value) {
+  if (value < 0.6) {
+    return "NO";
+  } else if (value < 0.8) {
+    return "Probably";
+  } else {
+    return "YES"
+  }
+};
+
+function Visit(index, response) {
+  this.index = index;
+  this.visitorId = response.visitorId;
+  this.incognito = response.incognito;
+  this.formattedIncognito = formatBool(response.incognito);
+  this.formattedBot = formatBool(response.botProbability);
+  this.ip = response.ip;
+  this.formattedLocation = formatIpLocation(response.ipLocation);
+  this.formattedBrowser = formatBrowser(response.browserDetails || response);
+  var pageViewTime = new Date(response.time);
+  this.formattedTimeAgo = timeago().format(response.time) + ", " +
+    pageViewTime.toLocaleString();
+  this.visitorFound = response.visitorFound;
+  this.deviceId = "Coming soon...";
+  this.lng = response.ipLocation.longitude;
+  this.lat = response.ipLocation.latitude;
+  // UI variables
+  this.collapsed = true;
+  this.mapInitialized = false;
+  this.mapContainerId = "map-container-" + index;
+}
+
+Visit.prototype = {
+  onArrowClick: function () {
+    this.collapsed = !this.collapsed;
+    if (!this.mapInitialized) {
+      var visit = this;
+      setTimeout(function(){
+        visit.initMap();
+      }, 100);
+    }
+  },
+  initMap: function () {
+    var styleId = this.incognito ? 'dark-v10' : 'streets-v11';
+    var map = new mapboxgl.Map({
+      container: this.mapContainerId,
+      style: 'mapbox://styles/mapbox/' + styleId,
+      center: [this.lng, this.lat], // starting position [lng, lat]
+      zoom: 5 // starting zoom
+    });
+    this.mapInitialized = true;
+  }
 }
 
 function fpLoaded(fp) {
@@ -65,73 +108,37 @@ function fpLoaded(fp) {
 }
 
 function initApp(response) {
+  var visit = new Visit(0, response);
   var app = new Vue({
     el: "#demo",
     data: {
-      visitorId: response.visitorId,
-      response: response,
+      currentVisit: visit,
       visits: []
     },
     methods: {
-      fullLocation: function () {
-        return formatIpLocation(this.response.ipLocation);
-      },
-      fullBrowser: function () {
-        return formatBrowser(this.response);
-      },
-      incognito: function () {
-        return this.response.incognito;
-      },
-      fullIncognito: function () {
-        return formatBool(this.response.incognito);
-      },
-      bot: function () {
-        if (this.response.botProbability < 0.6) {
-          return "NO";
-        } else if (this.response.botProbability < 0.8) {
-          return "Probably";
-        } else {
-          return "YES"
-        }
-      },
-      ipAddress: function () {
-        return this.response.ip;
-      },
-      deviceId: function () {
-        return "Coming soon...";
-      },
-      showDemoHistory: function () {
-        if (this.response.visitorFound) {
-          return !this.response.visitorFound;
-        }
-        return false;
-      },
       showHistory: function () {
         return this.visits.length > 0;
       }
     }
   });
   if (response.visitorFound) {
-    var url = "https://v2.api.fpjs.io/visitors/" + response.visitorId + "/?token=sandbox-api-key&limit=5";
+    var url = "https://v2.api.fpjs.io/visitors/" + response.visitorId + "/?token=sandbox-api-key&limit=6";
     $.getJSON(url, function (visitsResponse) {
-      for(var i = 1; i < visitsResponse.visits.length; i++){
-        app.visits.push(new Visit(visitsResponse.visits[i]));
+      for (var i = 1; i < visitsResponse.visits.length; i++) {
+        app.visits.push(new Visit(i, visitsResponse.visits[i]));
       }
     });
   }
-  var coords = {
-    lng: response.ipLocation.longitude,
-    lat: response.ipLocation.latitude
-  };
-  initMap(coords);
+  initMap(visit);
 }
 
-function initMap(coords) {
+function initMap(visit) {
+  var styleId = visit.incognito ? 'dark-v10' : 'streets-v11';
   var map = new mapboxgl.Map({
-    container: 'map-container', // container id
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [coords.lng, coords.lat], // starting position [lng, lat]
-    zoom: 5 // starting zoom
+    container: 'current-visit-map-container', 
+    style: 'mapbox://styles/mapbox/' + styleId,
+    center: [visit.lng, visit.lat], 
+    zoom: 3 // smaller is wider
   });
   var size = 200;
 
@@ -183,8 +190,8 @@ function initMap(coords) {
   };
   map.on('load', function () {
     map.fitBounds([
-      [coords.lng - 1, coords.lat - 1],
-      [coords.lng + 1, coords.lat + 1]
+      [visit.lng - 1, visit.lat - 1],
+      [visit.lng + 1, visit.lat + 1]
     ]);
     // map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
 
